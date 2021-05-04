161eaf44adfc3a0596ef0741ef71a44c019710d3
Pedro Batista <pedrobatista.420@gmail.com>
2021-05-04 17:15:37 +0100

    starting setup. added 2 components, header andcart button

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,23 @@
+import { Fragment } from 'react';
+
+import HeaderCartButton from './HeaderCartButton';
+
+import mealsImage from '../../assets/meals.jpeg';
+
+import classes from './Header.module.css';
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Food Reacts</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt="Hungry?" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
