034f106fbd8de62f7da3c2eee77ba598085f5ec7
Pedro Batista <pedrobatista.420@gmail.com>
2021-05-10 15:58:15 +0100

    added a Modal to show the order receipt

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,26 +1,28 @@
+import Modal from '../UI/Modal';
+
 import classes from './Cart.module.css';
 
 const Cart = (props) => {
   const cartItems = (
     <ul className={classes['cart-items']}>
       {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
-        <li>{item.name}</li>
+        <li key={item.id}>{item.name}</li>
       ))}
     </ul>
   );
 
   return (
-    <div>
+    <Modal>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>56.89</span>
       </div>
       <div className={classes.actions}>
-        <span className={classes['button--alt']}>Close</span>
-        <span className={classes.button}>Order</span>
+        <button className={classes['button--alt']}>Close</button>
+        <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
